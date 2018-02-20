9ec8b1450aabe3e138f08bdf20dad29e586afd1e
Ajinkya Munge <amunge3@gatech.edu>
2018-02-19 20:59:32 -0500

    Added zoning code but did not change the net start/end indexes

--- RTK-NA/RTKNA.cpp
+++ RTK-NA/RTKNA.cpp
@@ -9,12 +9,15 @@
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <set>
+#include <map>
 
 using namespace std;
 vector<int> top;
 vector<int> bot;
-vector<vector<int>> col;
 
+map<int, int> netcol_max; //net number, max col
+map<int, int> netcol_min; //net no , min col
 
 struct net {
 	vector<int> indexes;
@@ -27,6 +30,8 @@ struct net {
 net * first;
 vector<net*> netlist;
 
+
+
 void parse(string);
 void arraytonet();
 
@@ -34,6 +39,97 @@ int main(int argc, char* argv[])
 {
 	parse(argv[0]);
 	arraytonet();
+
+	//Zoning Code
+	int maxnum = 0;
+	for (int i = 0; i < static_cast<int>(Top.size()); i++) {
+		maxnum = (Top[i] > maxnum) ? Top[i] : maxnum;
+		maxnum = (Bottom[i] > maxnum) ? Bottom[i] : maxnum;
+	}
+	for (int k = 1; k <= static_cast<int>(Top.size()); k++) {
+		if (netcol_max.find(Top[k - 1]) == netcol_max.end()) {
+			//cout << "Not found";
+			netcol_max[Top[k - 1]] = k;
+			//cout << netcol_max[Top[k - 1]];
+		}
+		else if (k>netcol_max[Top[k - 1]]) {
+			netcol_max[Top[k - 1]] = k;
+			//cout << "Found";
+		}
+		if (netcol_max.find(Bottom[k - 1]) == netcol_max.end()) {
+			//cout << "Not found";
+			netcol_max[Bottom[k - 1]] = k;
+			//cout << netcol_max[Top[k - 1]];
+		}
+		else if (k>netcol_max[Bottom[k - 1]]) {
+			netcol_max[Bottom[k - 1]] = k;
+			//cout << "Found";
+		}
+
+		if (netcol_min.find(Top[k - 1]) == netcol_min.end()) {
+			//cout << "Not found";
+			netcol_min[Top[k - 1]] = k;
+			//cout << netcol_min[Top[k - 1]];
+		}
+		else if (k<netcol_min[Top[k - 1]]) {
+			netcol_min[Top[k - 1]] = k;
+			//cout << "Found";
+		}
+		if (netcol_min.find(Bottom[k - 1]) == netcol_min.end()) {
+			//cout << "Not found";
+			netcol_min[Bottom[k - 1]] = k;
+			//cout << netcol_min[Top[k - 1]];
+		}
+		else if (k<netcol_min[Bottom[k - 1]]) {
+			netcol_min[Bottom[k - 1]] = k;
+			//cout << "Found";
+		}
+	}
+
+	vector< vector<int> > col;
+	for (int i1 = 0; i1 < static_cast<int>(Top.size()); i1++) {
+		vector<int> row; // Create an empty row
+		for (int j1 = 1; j1 < maxnum + 1; j1++) {
+			if (i1 <= netcol_max[j1] && i1 >= netcol_min[j1]) {
+				row.push_back(j1); // Add an element (column) to the row
+			}
+		}
+		col.push_back(row); // Add the row to the main vector
+	}
+
+	int p = 0;
+
+	vector<vector<int>> zone(static_cast<int>(Top.size()), vector<int>(5, 0));
+	for (int k1 = 1; k1 < static_cast<int>(Top.size()) - 1; k1++) {
+		//if (k1== static_cast<int>(Top.size()-1) {
+
+		if ((includes(col[k1].begin(), col[k1].end(), col[k1 + 1].begin(), col[k1 + 1].end())) || (includes(col[k1 + 1].begin(), col[k1 + 1].end(), col[k1].begin(), col[k1].end()))) {
+			//cout << "Subset";
+			std::vector<int> temp_colunion(static_cast<int>(col[k1].size()) * 10);
+			std::vector<int>::iterator it;
+			it = set_union(col[k1].begin(), col[k1].end(), col[k1 + 1].begin(), col[k1 + 1].end(), temp_colunion.begin());
+			temp_colunion.resize(it - temp_colunion.begin());
+
+			zone[p] = (includes(zone[p].begin(), zone[p].end(), temp_colunion.begin(), temp_colunion.end())) ? zone[p] : temp_colunion;
+
+		}
+		else {
+			//cout << "\n not a subset";
+			//p = (sub_count > 0) ? (p + 1) : p;
+			p += 1;
+			zone[p] = col[k1 + 1];
+		}
+
+	}
+	zone.erase(zone.begin() + p + 1, zone.end());
+	//cout << "P is " << p << endl;
+	for (int k1 = 0; k1 < static_cast<int>(zone.size()); k1++) {
+		cout << endl;
+		cout << "Zone no" << (k1 + 1) << endl;
+		for (int k2 = 0; k2 < static_cast<int>(zone[k1].size()); k2++) {
+			cout << " " << zone[k1][k2];
+		}
+	}
     return 0;
 }
 
