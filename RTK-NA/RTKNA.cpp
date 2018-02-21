11a1e94b9f15ac1933a01d9c773127a5157fd652
Nicholas Landi <ntlandi@gmail.com>
2018-02-20 19:16:59 -0500

    fixed parsing, finished netlist, all tested

--- RTK-NA/RTKNA.cpp
+++ RTK-NA/RTKNA.cpp
@@ -24,11 +24,14 @@ struct net {
 	int startind, endind;
 };
 
+bool sortNet(const net *a, const net *b);
+
 net * first;
 vector<net*> netlist;
 
 void parse(string);
 void arraytonet();
+int findExistingNet(int);
 
 int main(int argc, char* argv[])
 {
@@ -39,41 +42,45 @@ int main(int argc, char* argv[])
 
 void parse(string fileloc) {
 	ifstream file;
-	file.open(fileloc);
+	file.open("C:\\Users\\Nick Landi\\Desktop\\test.txt");
 
-	string line = file.getline;
-	uint64_t index = 0;
-	uint64_t start = 0;
+	string line;
+	getline(file, line);
+	size_t index = 0;
+	size_t previndex = 0;
 
-	while (index < line.length) {
-		index = line.find(" ", (size_t)start);
-		top.push_back(stoi(line.substr(start, index - start - 1)));
-		start = index + 1;
+	while (index < line.length()) {
+		index = line.find(" ", (size_t)previndex);
+		top.push_back(stoi(line.substr(previndex, index - previndex)));
+		previndex = index + 1;
 	}
 
-	line = file.getline;
-	while (index < line.length) {
-		index = line.find(" ", (size_t)start);
-		bot.push_back(stoi(line.substr(start, index - start - 1)));
-		start = index + 1;
+	index = 0;
+	previndex = 0;
+	getline(file, line);
+	while (index < line.length()) {
+		index = line.find(" ", (size_t)previndex);
+		bot.push_back(stoi(line.substr(previndex, index - previndex )));
+		previndex = index + 1;
 	}
 }
 
 void arraytonet() {
 	int previndex;
 	//top part
-	for (int i = 0; i < top.size; i++) {
+	for (int i = 0; i < top.size(); i++) {
 		net *next = new net;
 		int nextnet = top.at(i);
+		int net = findExistingNet(nextnet);
 
-		auto it = (find_if(netlist.begin(), netlist.end(), [&nextnet](const net& p) {return p.netnum == nextnet; }));
+		//auto it = (find_if(netlist.begin(), netlist.end(), [&nextnet](const net& p) {return p.netnum == nextnet; }));
 		if (nextnet == 0) {
 			continue;
 		}
-		else if (it != netlist.end) {
-			(*it)->indexes.push_back(i);
-			(*it)->directions.push_back(true);
-			(*it)->endind = i;
+		else if (net != -1) {
+			netlist[net]->indexes.push_back(i);
+			netlist[net]->directions.push_back(true);
+			netlist[net]->endind = i;
 		}
 		else {
 			next->startind = i;
@@ -81,7 +88,48 @@ void arraytonet() {
 			next->endind = i;
 			next->indexes.push_back(i);
 			next->netnum = nextnet;
+			netlist.push_back(next);
 		}
 	}
 	//bottom part
+	for (int i = 0; i < bot.size(); i++) {
+		net *next = new net;
+		int nextnet = bot.at(i);
+		int net = findExistingNet(nextnet);
+
+		//auto it = (find_if(netlist.begin(), netlist.end(), [&nextnet](const net& p) {return p.netnum == nextnet; }));
+		if (nextnet == 0) {
+			continue;
+		}
+		else if (net != -1) {
+			netlist[net]->indexes.push_back(i);
+			netlist[net]->directions.push_back(false);
+			netlist[net]->endind = i;
+		}
+		else {
+			next->startind = i;
+			next->directions.push_back(false);
+			next->endind = i;
+			next->indexes.push_back(i);
+			next->netnum = nextnet;
+			netlist.push_back(next);
+		}
+	}
+
+	sort(netlist.begin(), netlist.end(), sortNet);
+}
+
+bool sortNet(const net *a, const net *b) {
+	return a->netnum < b->netnum;
+}
+
+int findExistingNet(int net) {
+
+	for (int i = 0; i < netlist.size(); i++) {
+		if (netlist[i]->netnum == net) {
+			return i;
+		}
+	}
+
+	return -1;
 }
